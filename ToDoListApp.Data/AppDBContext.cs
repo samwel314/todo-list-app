68b496b742fe4c534ca0ab37d5fc10285bac9ff4
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-02-05 02:36:17 +0200

    feat : add User Model To App

--- ToDoListApp.Data/AppDBContext.cs
+++ ToDoListApp.Data/AppDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,7 +7,7 @@ using ToDoListApp.Models;
 
 namespace ToDoListApp.Data
 {
-    public class AppDBContext : DbContext
+    public class AppDBContext : IdentityDbContext<User>
     {
         public AppDBContext(DbContextOptions options) : base(options)
         {
---
+++ ToDoListApp.Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ToDoListApp.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;  
+        public string LastName { get; set; } = null!;   
+    }
+}
